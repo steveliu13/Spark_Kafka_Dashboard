74df4faba35c5dd00cebff21b7a944f9fd096dcb
liuyudeng <steveliu13@163.com>
2020-04-01 00:28:41 +0800

    删除多余文件

--- test/test_consumer.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from scripts.consumer import Consumer
-
-
-class Test(TestCase):
-    def test_consumer(self):
-        for msg in Consumer():
-            print(msg)
--- test/test_producer.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from scripts.producer import Producer
-
-
-class Test(TestCase):
-    def test_producer(self):
-        Producer()
